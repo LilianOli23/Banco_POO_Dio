8f2483fce0489dc35a3dcae09e16f90e3d126d61
liliandeoliveirasilva <lilian.agricola@gmail.com>
2021-12-02 11:08:27 -0300

    Projeto entregue para o desafio do bootcamp Amdocs.

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,16 +1,44 @@
 
+public class ContaCorrente {
 
+    String numero;
+    String agencia;
+    boolean especial;
+    double limiteEspecial;
+    double saldo;
 
-public class ContaCorrente extends Conta {
+    boolean realizarSaque(double quantiaASacar){
 
-    public ContaCorrente(Cliente cliente) {
-        super(cliente);
+        //tem saldo na conta
+        if (saldo >= quantiaASacar){
+            saldo -= quantiaASacar;
+            return true;
+        } else { //não tem saldo na conta
+            if (especial) {
+                //verificar se o limite especial tem saldo
+                double limite = limiteEspecial + saldo;
+                if (limite >= quantiaASacar){
+                    saldo -= quantiaASacar;
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false; //não é especial e não tem saldo suficiente
+            }
+        }
     }
 
-    @Override
-    public void imprimirExtrato() {
-        System.out.println("=== Extrato Conta Corrente ===");
-        super.imprimirInfosComuns();
+    void depositar(double valorDepositado){
+
+        saldo += valorDepositado;
     }
 
+    void consultarSaldo(){
+        System.out.println("Saldo atual da conta = " + saldo);
+    }
+
+    boolean verificarUsoChequeEspecial(){
+        return saldo < 0;
+    }
 }
